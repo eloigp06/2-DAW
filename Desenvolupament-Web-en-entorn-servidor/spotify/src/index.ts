9e07cfd376beddda5bece4977af0a35e42348b9a
Eloi Gomez Prada <eloigp06@gmail.com>
2026-09-21 18:50:06 +0200

    get tracks

--- Desenvolupament-Web-en-entorn-servidor/spotify/src/index.ts
+++ Desenvolupament-Web-en-entorn-servidor/spotify/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import { APICONFIG } from "./config/apiConfig";
 import { tracks } from "./data/track/track";
+import { Track } from "./interfaces/track/track";
 
 const app: Express = express();
 
@@ -10,6 +11,18 @@ app.get("/", (_req: Request, res: Response) => { // _req → petició rebuda per
 
 app.get("/tracks", (_req: Request, res: Response) => { // _req → petició rebuda però no utilitzada
   return res.status(200).json(tracks);
+});
+
+app.get("/tracks/:id", (req: Request, res: Response) => { 
+  const idTrack: string = req.params.id as string;
+
+  const track:  Track[] = tracks.filter(
+    (t: Track) => {return t.id === idTrack}
+  );
+  if (track.length === 0){
+    return res.status(404).json({message: `Track ${idTrack} not found`})
+  }
+  return res.status(200).json(track);
 });
 
 app.listen(APICONFIG.port, APICONFIG.host, () => {
