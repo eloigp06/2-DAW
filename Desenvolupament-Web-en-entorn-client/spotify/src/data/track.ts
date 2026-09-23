8e4acd9dd9651950b972a58618b8cefb7cf0369a
Eloi Gomez Prada <eloigp06@gmail.com>
2026-09-23 20:27:03 +0200

    App buscadora de cançons

---
+++ Desenvolupament-Web-en-entorn-client/spotify/src/data/track.ts
@@ -0,0 +1,28 @@
+import type { Track } from "../interface/track";
+
+export const tracks: Track[] = [
+    {
+        id: "2B-CA",
+        title: "HO MY GOOD",
+        artist: "U2",
+        duration: 90
+    },
+    {
+        id: "2B-CA",
+        title: "Chicago",
+        artist: "Michael Jackson",
+        duration: 190
+    },
+    {
+        id: "2B-CA",
+        title: "Manolete",
+        artist: "Michael Jackson",
+        duration: 120
+    },
+    {
+        id: "2B-CA",
+        title: "Pepinillo",
+        artist: "Michael Jackson",
+        duration: 190
+    }
+];
