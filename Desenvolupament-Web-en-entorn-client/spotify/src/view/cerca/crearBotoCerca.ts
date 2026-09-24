025fe5c83b5ca3003e22f6842870ea555dcc9cc3
Eloi Gomez Prada <eloigp06@gmail.com>
2026-09-24 19:56:00 +0200

    boto clicat

--- Desenvolupament-Web-en-entorn-client/spotify/src/view/cerca/crearBotoCerca.ts
+++ Desenvolupament-Web-en-entorn-client/spotify/src/view/cerca/crearBotoCerca.ts
@@ -3,6 +3,7 @@ export function crearBotoCerca(): HTMLButtonElement {
     const botoCerca: HTMLButtonElement = document.createElement("button")
 
     botoCerca.type = "button";
+    botoCerca.textContent = "Cerca";
     botoCerca.addEventListener("click", () => {
         console.log("boto clicat");
     });
