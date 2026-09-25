70fcbbe3a1b1851d6b6834e14ea52c321b46b28d
Eloi Gomez Prada <eloigp06@gmail.com>
2026-09-25 16:53:36 +0200

    uuid installed

--- Desenvolupament-Web-en-entorn-servidor/spotify/src/validators/track.validator.ts
+++ Desenvolupament-Web-en-entorn-servidor/spotify/src/validators/track.validator.ts
@@ -1,12 +1,13 @@
 import { Track } from "../interfaces/track/track";
 
 export function isValidTrack(track: Track): boolean{
+    let correcte: boolean = true;
 
-    if(track.artist == null || track.title == null || track.duration == null){
-        return false;
+    if(!track.artist || !track.title || !track.duration){
+        return false
     }
     if (track.artist.trim().length ===0 || track.title.trim().length===0 || track.duration<=0){
-        return false;
+        correcte =false;
     }
-    return true
+    return correcte
 }
